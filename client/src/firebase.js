156c05cb77e7e62126d3a9363c945352379e6a59
shashwat6204 <shashwatcse6204@gmail.com>
2024-07-12 01:17:46 +0530

    firebase changes

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -3,14 +3,15 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCpd4ogEG3F79YMSuvZDunuOKAnEFJ0XN8",
-    authDomain: "juridex-5d937.firebaseapp.com",
-    projectId: "juridex-5d937",
-    storageBucket: "juridex-5d937.appspot.com",
-    messagingSenderId: "382687835528",
-    appId: "1:382687835528:web:1c770f4f4bc066c670f63d",
-    measurementId: "G-EBR9FP39XW"
-  };
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
+};
+
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
